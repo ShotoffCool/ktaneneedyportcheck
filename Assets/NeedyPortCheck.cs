bdba55c0f0f56d910ea5a23c971eca3efa5ef26f
ShotoffCool <77933377+ShotoffCool@users.noreply.github.com>
2025-12-16 23:33:10 +0100

    module done

--- Assets/NeedyPortCheck.cs
+++ Assets/NeedyPortCheck.cs
@@ -8,31 +8,60 @@ using KModkit;
 using Rnd = UnityEngine.Random;
 using Math = ExMath;
 
-public class NeedyTemplate : MonoBehaviour
+public class NeedyPortCheck : MonoBehaviour
 {
-
     public KMBombInfo Bomb;
     public KMAudio Audio;
     public KMNeedyModule Needy;
 
     static int ModuleIdCounter = 1;
     int ModuleId;
-    private bool ModuleSolved;
+    private bool ModuleSolved = true;
+
+    public enum PortType
+    {
+        DVI = 0x01,
+        Parallel = 0x02,
+        PS2 = 0x04,
+        RJ45 = 0x08,
+        Serial = 0x10,
+        StereoRCA = 0x20
+    };
+
+    public KMSelectable[] buttons;
+
+    Dictionary<Component, PortType> ports;
+    List<Renderer> disabledMeshes = new List<Renderer>();
+
+    PortType currentPort;
+    Component portComponent;
+
+    static List<Component> occupiedComponents = new List<Component>();
 
     void Awake()
     { //Avoid doing calculations in here regarding edgework. Just use this for setting up buttons for simplicity.
         ModuleId = ModuleIdCounter++;
-        GetComponent<KMBombModule>().OnActivate += Activate;
         Needy.OnNeedyActivation += OnNeedyActivation;
         Needy.OnNeedyDeactivation += OnNeedyDeactivation;
         Needy.OnTimerExpired += OnTimerExpired;
-        /*
-        foreach (KMSelectable object in keypad) {
-            object.OnInteract += delegate () { keypadPress(object); return false; };
-        }
-        */
+        Needy.OnActivate += Activate;
 
-        //button.OnInteract += delegate () { buttonPress(); return false; };
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int i2 = i;
+            buttons[i].OnInteract += delegate ()
+            {
+                Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, buttons[i2].transform);
+                buttons[i2].AddInteractionPunch();
+                if (ModuleSolved)
+                    return false;
+                if ((int)currentPort == 1 << i2)
+                    OnNeedyDeactivation();
+                else
+                    Strike();
+                return false;
+            };
+        }
 
     }
 
@@ -44,21 +73,47 @@ public class NeedyTemplate : MonoBehaviour
     void Activate()
     { //Shit that should happen when the bomb arrives (factory)/Lights turn on
 
+        ports = NeedyPortCheckReflectionTools.GetPortsMapped(this);
+
+        foreach (KeyValuePair<Component, PortType> kvp in ports)
+        {
+            Debug.LogFormat("[needyPortCheck {0}] Key Value Pair" + kvp.Value, ModuleId);
+        }
     }
 
     protected void OnNeedyActivation()
     { //Shit that happens when a needy turns on.
+        ModuleSolved = false;
+        List<Component> freeComponents = ports.Keys.Where(x => !occupiedComponents.Contains(x)).ToList();
 
+        if (freeComponents.Count == 0)
+        {
+            OnNeedyDeactivation();
+            return;
+        }
+
+        portComponent = freeComponents.PickRandom();
+        currentPort = ports[portComponent];
+
+        occupiedComponents.Add(portComponent);
+        hidePort(portComponent);
+        
+        Debug.LogFormat("[needyPortCheck {0}] Hiding " + currentPort, ModuleId);
     }
 
     protected void OnNeedyDeactivation()
     { //Shit that happens when a needy turns off.
+        ModuleSolved = true;
+
         Needy.OnPass();
+        showPort();
+        occupiedComponents.Remove(portComponent);
     }
 
     protected void OnTimerExpired()
     { //Shit that happens when a needy turns off due to running out of time.
-
+        Strike();
+        OnNeedyDeactivation();
     }
 
     void Start()
@@ -73,7 +128,30 @@ public class NeedyTemplate : MonoBehaviour
 
     void Strike()
     {
-        GetComponent<KMBombModule>().HandleStrike();
+        Needy.HandleStrike();
+    }
+
+    void hidePort(Component portToHide)
+    {
+        foreach(Renderer mesh in portToHide.GetComponentsInChildren<Renderer>())
+        {
+            if (!mesh.enabled)
+            {
+                continue;
+            }
+
+            mesh.enabled = false;
+            disabledMeshes.Add(mesh);
+        }
+    }
+
+    void showPort()
+    {
+        foreach (Renderer mesh in disabledMeshes)
+        {
+            mesh.enabled = true;
+        }
+        disabledMeshes.Clear();
     }
 
 #pragma warning disable 414
---
+++ Assets/NeedyPortCheckReflectionTools.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedyPortCheckReflectionTools
+{
+
+    public static Type FetchPortType()
+    {
+        return Type.GetType("Port, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+    }
+
+    public static IEnumerable<Component> GetAllPorts(Component module)
+    {
+        Transform transform = module.transform;
+        Transform parentTransform;
+        KMBomb bomb = transform.GetComponentInParent<KMBomb>();
+        if (bomb != null)
+            parentTransform = bomb.transform;
+        else
+        {
+            parentTransform = transform;
+            while (parentTransform.transform.parent != null && parentTransform.GetComponent("Bomb") == null)
+                parentTransform = parentTransform.parent;
+        }
+        return parentTransform.GetComponentsInChildren(FetchPortType());
+    }
+
+    public static NeedyPortCheck.PortType GetPortType(Component port)
+    {
+        try
+        {
+            return (NeedyPortCheck.PortType)FetchPortType()
+                .GetField("PortType", BindingFlags.Instance | BindingFlags.Public)
+                .GetValue(port);
+        }
+        catch { return 0; }
+    }
+
+    public static Dictionary<Component, NeedyPortCheck.PortType> GetPortsMapped(Component module)
+    {
+        Dictionary<Component, NeedyPortCheck.PortType> portMap = new Dictionary<Component, NeedyPortCheck.PortType>();
+        foreach (Component port in GetAllPorts(module))
+            portMap.Add(port, GetPortType(port));
+
+        return portMap;
+    }
+}
